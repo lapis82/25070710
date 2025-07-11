dcf808aef63283746874f3a73593af8c51b4919d
lapis82 <classica@snu.ac.kr>
2025-07-11 18:10:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 import streamlit as st
-st.title('sj의 두번째 웹앱')
-st.write('복습')
+st.title('나의 첫 웹 앱 만들기')
+name= st.text_input('이름:')
+menu= st.selectbox('선호 메뉴:', ['망고빙수', '아몬드봉봉'])
+if st.button('인사말 생성'):
+  st.write( name+님, 당신이 선호하는 디저트는 '+menu+'이군요. 저도 좋아요.')
